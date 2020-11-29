07410d2a671220d361136e65de7eac4f55b31eef
Davide Canton <davide.canton5@gmail.com>
2020-11-29 11:56:45 +0100

    eslint

--- index.js
+++ index.js
@@ -1,77 +1,89 @@
-const semver = require("semver");
-const yargs = require("yargs/yargs");
-const { hideBin } = require("yargs/helpers");
-const fetch = require("node-fetch");
-const _ = require("lodash");
+#!/usr/bin/env node
+
+const semver = require('semver');
+const yargs = require('yargs/yargs');
+const {hideBin} = require('yargs/helpers');
+const fetch = require('node-fetch');
+const _ = require('lodash');
 
 const argv = yargs(hideBin(process.argv))
-  .command("$0", "the default command", (yargs) => {
-    yargs.positional("package", {
-      describe: "package name",
-      type: "string",
-    });
-  })
-  .options({
-    range: {
-      alias: "r",
-      type: "string",
-      default: "",
-      description: "Semver range",
-    },
-    onlyMajor: {
-      alias: "m",
-      type: "boolean",
-      default: false,
-      description: "Inspect only major",
-    },
-    onlyStable: {
-      alias: "s",
-      type: "boolean",
-      default: false,
-      description: "Inspect only stable",
-    },
-  })
-  .help().argv;
+    .command(
+        '$0',
+        'Queries the provided registry and returns all the dependencies for each version.',
+        (yargs) => {
+            yargs.positional('package', {
+                describe: 'package identifier',
+                type: 'string',
+            });
+        },
+    )
+    .options({
+        range: {
+            alias: 'r',
+            type: 'string',
+            default: '',
+            description: 'Semver range to retrieve.',
+        },
+        onlyMajor: {
+            alias: 'm',
+            type: 'boolean',
+            default: false,
+            description: 'Inspect only the latest version for each major.',
+        },
+        onlyStable: {
+            alias: 's',
+            type: 'boolean',
+            default: false,
+            description:
+                'Inspect only stable versions (no alpha, beta, prereleases).',
+        },
+        registry: {
+            type: 'string',
+            default: 'https://registry.npmjs.org',
+            description: 'NPM registry to query.',
+        },
+    })
+    .help().argv;
 
 async function main(args) {
-  const [package] = args._;
-  const { onlyMajor, onlyStable, range } = args;
+    const [package] = args._;
+    const {registry, onlyMajor, onlyStable, range} = args;
 
-  const { versions, time } = await fetch(
-    `https://registry.npmjs.org/${package}`
-  ).then((v) => v.json());
+    const {versions, time} = await fetch(`${registry}/${package}`).then((v) =>
+        v.json(),
+    );
 
-  Object.keys(time).forEach((k) => (time[k] = new Date(time[k])));
+    Object.keys(time).forEach((k) => (time[k] = new Date(time[k])));
 
-  let allVersions = _.chain(Object.keys(versions)).orderBy((v) => time[v], [
-    "desc",
-  ]);
-
-  if (range)
-    allVersions = allVersions.filter((v) => semver.satisfies(v, range));
-
-  if (onlyStable)
-    allVersions = allVersions.filter((v) => !semver.prerelease(v));
-
-  if (onlyMajor) {
-    allVersions = allVersions
-      .groupBy((v) => semver.major(v))
-      .mapValues((vv) => vv[0])
-      .values();
-  }
-
-  allVersions = allVersions.value();
-
-  const out = {};
-  for (const version of allVersions) {
-    out[version] = _.pick(versions[version], [
-      "dependencies",
-      "devDependencies",
-      "peerDependencies",
+    let allVersions = _.chain(Object.keys(versions)).orderBy((v) => time[v], [
+        'desc',
     ]);
-  }
 
-  console.log(out);
+    if (range)
+        allVersions = allVersions.filter((v) => semver.satisfies(v, range));
+
+    if (onlyStable)
+        allVersions = allVersions.filter((v) => !semver.prerelease(v));
+
+    if (onlyMajor) {
+        allVersions = allVersions
+            .groupBy((v) => semver.major(v))
+            .mapValues((vv) => vv[0])
+            .values();
+    }
+
+    allVersions = allVersions.value();
+
+    const out = {};
+    for (const version of allVersions) {
+        out[version] = _.pick(versions[version], [
+            'dependencies',
+            'devDependencies',
+            'peerDependencies',
+        ]);
+    }
+
+    console.log(out);
 }
 
 main(argv);
